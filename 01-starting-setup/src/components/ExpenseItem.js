79999a25214d1ae4956fe841d1e2f16f6ed91e4d
LanceStasinski <lance.stasinski@gmail.com>
2021-07-23 17:16:27 -0400

    refactor: Refactor expense date into seperate component

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,13 +1,12 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 
+
 function ExpenseItem(props) {
-  const expenseDate = new Date(2021, 7, 23);
-  const expenseTitle = 'Head First JavaScript Programming';
-  const expenseAmount = 35;
 
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
