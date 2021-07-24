a53ce1395205afbf509998b76f62bc276b8d6097
LanceStasinski <lance.stasinski@gmail.com>
2021-07-24 09:52:35 -0400

    feat: Create Expenses component

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,39 +1,36 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
     {
+      id: "e1",
       title: "Head First JavaScript Programming",
       amount: 35,
       date: new Date(2021, 7, 23),
     },
-    { title: "White Board", amount: 100, date: new Date(2020, 6, 14) },
-    { title: "Udacity FEND Course", amount: 350, date: new Date(2021, 3, 15) },
-    { title: "Udemy React Course", amount: 135, date: new Date(2021, 7, 23) },
+    {
+      id: "e2",
+      title: "White Board",
+      amount: 100,
+      date: new Date(2020, 6, 14),
+    },
+    {
+      id: "e3",
+      title: "Udacity FEND Course",
+      amount: 350,
+      date: new Date(2021, 3, 15),
+    },
+    {
+      id: "e4",
+      title: "Udemy React Course",
+      amount: 135,
+      date: new Date(2021, 7, 23),
+    },
   ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <Expenses data={expenses} />
     </div>
   );
 }
