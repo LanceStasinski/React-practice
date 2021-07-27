502fe45d374e6658389117dac3ce917416ed726b
LanceStasinski <lance.stasinski@gmail.com>
2021-07-27 11:25:28 -0400

    feat: Add form

--- user-info/src/components/InputForm.js
+++ user-info/src/components/InputForm.js
@@ -4,6 +4,15 @@ import styles from './InputForm.module.css'
 
 const InputForm = () => {
 
+  return (
+    <form>
+      <label>Username</label>
+      <input type='text' />
+      <label>Age (years)</label>
+      <input type='number' min='1' step='1' />
+      <button type='submit'>Add User</button>
+    </form>
+  )
 }
 
 export default InputForm;
