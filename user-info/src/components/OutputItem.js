b421b26eaea2540cc5b3f5d7e2c1a2a16b1ef4fc
LanceStasinski <lance.stasinski@gmail.com>
2021-07-27 15:30:27 -0400

    chore: Reset user-info for tutorial

--- user-info/src/components/OutputItem.js
+++
@@ -1,7 +0,0 @@
-import styles from './OutputItem.module.css'
-
-const OutputItem = (props) => {
-  return <li className={styles['output-item']}>{`${props.name} (${props.age} years old)`}</li>
-}
-
-export default OutputItem
