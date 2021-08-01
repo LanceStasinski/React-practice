6584bd614dd0aca8da88946773b1b5cf86e9b149
LanceStasinski <lance.stasinski@gmail.com>
2021-08-01 16:17:39 -0400

    feat: Replace email and password fields with reusable input component

--- login-page/src/components/MainHeader/Navigation.js
+++ login-page/src/components/MainHeader/Navigation.js
@@ -20,7 +20,7 @@ const Navigation = () => {
         )}
         {ctx.isLoggedIn && (
           <li>
-            <button onClick={ctx.onLogout}>Logout</button>
+            <button onClick={ctx.onLogOut}>Logout</button>
           </li>
         )}
       </ul>
