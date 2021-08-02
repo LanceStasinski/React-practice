5eabcaf9f56e5f632e3f93a9184f54eb82a14e58
LanceStasinski <lance.stasinski@gmail.com>
2021-08-02 14:35:56 -0400

    feat: Add AvailableMeals component

--- food-order-app/src/components/Meals/Meals.js
+++ food-order-app/src/components/Meals/Meals.js
@@ -1,9 +1,13 @@
-import MealsSummary from "./MealsSummary"
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
 
-const Meals = props => {
-  return(
-    <MealsSummary />
-  )
-}
+const Meals = (props) => {
+  return (
+    <>
+      <MealsSummary />
+      <AvailableMeals />
+    </>
+  );
+};
 
 export default Meals;
