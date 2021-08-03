412127eefad247b2f4f7770fe050ce88bda27b7a
LanceStasinski <lance.stasinski@gmail.com>
2021-08-02 20:25:31 -0400

    feat: Wrap all components with context

--- food-order-app/src/App.js
+++ food-order-app/src/App.js
@@ -3,26 +3,27 @@ import React, { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
-  const [cartIsShown, setCartIsShown] = useState(false)
+  const [cartIsShown, setCartIsShown] = useState(false);
 
   const showCartHandler = () => {
-    setCartIsShown(true)
-  }
+    setCartIsShown(true);
+  };
 
   const hideCartHandler = () => {
-    setCartIsShown(false)
-  }
+    setCartIsShown(false);
+  };
 
   return (
-    <>
-      {cartIsShown && <Cart onClose={hideCartHandler}/>}
-      <Header onShowCart={showCartHandler}/>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
