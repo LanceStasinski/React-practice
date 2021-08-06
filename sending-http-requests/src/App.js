c91436cbf9d0d4457a2cb25e4fc66379ffa45797
LanceStasinski <lance.stasinski@gmail.com>
2021-08-06 08:32:33 -0400

    refactor: Display content conditionally using single variable

--- sending-http-requests/src/App.js
+++ sending-http-requests/src/App.js
@@ -19,7 +19,6 @@ function App() {
 
       const data = await response.json();
 
-
       const transformedMovies = data.results.map((movieData) => {
         return {
           id: movieData.episode_id,
@@ -35,16 +34,27 @@ function App() {
     setIsLoading(false);
   };
 
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>
+  }
+
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && !error && <p>No Movies Found</p>}
-        {isLoading && <p>Loading...</p>}
-        {!isLoading && error && <p>{error}</p>}
+        {content}
       </section>
     </React.Fragment>
   );
