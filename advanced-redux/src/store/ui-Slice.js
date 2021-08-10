3bb918309c2bbf509b431d76fbccfabd39f064cb
LanceStasinski <lance.stasinski@gmail.com>
2021-08-10 12:47:31 -0400

    feat: Add UI state slice

---
+++ advanced-redux/src/store/ui-Slice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const uiSlice = createSlice({
+  name: 'ui',
+  initialState: { cartIsVisible: false },
+  reducers: {
+    toggle(state) {
+      state.cartIsVisible = !state.cartIsVisible;
+    }
+  }
+})
+
+const uiActions = uiSlice.actions;
+export default uiSlice;
