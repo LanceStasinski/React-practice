52203debf120d060125e83250edaa73da6c28a7b
LanceStasinski <lance.stasinski@gmail.com>
2021-08-15 18:50:39 -0400

    feat: Define routes

--- router-project/src/App.js
+++ router-project/src/App.js
@@ -1,8 +1,23 @@
+import { Route, Switch } from 'react-router-dom'
+
+import NewQuote from './pages/NewQuote';
+import QuoteDetail from './pages/QuoteDetail';
+import AllQuotes from './pages/AllQuotes';
+
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Switch>
+      <Route path='/quotes' exact>
+        <AllQuotes />
+      </Route>
+      <Route path='/quotes/:quoteId'>
+        <QuoteDetail />
+      </Route>
+      <Route path='/new-quote'>
+        <NewQuote />
+      </Route>
+    </Switch>
   );
 }
 
---
+++ router-project/src/pages/NewQuote.js
@@ -0,0 +1,5 @@
+const NewQuote = (props) => {
+  return <h1>New Quotes Page</h1>
+}
+
+export default NewQuote;
