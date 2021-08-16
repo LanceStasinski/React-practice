5f6749c093e1aafb2ddef028f7683e5d930ace19
LanceStasinski <lance.stasinski@gmail.com>
2021-08-16 14:11:24 -0400

    feat: Add dummy data to display QuoteList

--- router-project/src/App.js
+++ router-project/src/App.js
@@ -3,23 +3,26 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import NewQuote from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
 import AllQuotes from "./pages/AllQuotes";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact>
-        <Redirect to="/quotes" />
-      </Route>
-      <Route path="/quotes" exact>
-        <AllQuotes />
-      </Route>
-      <Route path="/quotes/:quoteId">
-        <QuoteDetail />
-      </Route>
-      <Route path="/new-quote">
-        <NewQuote />
-      </Route>
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
