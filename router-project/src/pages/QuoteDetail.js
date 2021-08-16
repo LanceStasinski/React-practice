158c3d0f4c7a06d85f6d73d2c834a16d08e533a1
LanceStasinski <lance.stasinski@gmail.com>
2021-08-16 14:33:30 -0400

    feat: Add link to QuoteDetail from AllQuotes

--- router-project/src/pages/QuoteDetail.js
+++ router-project/src/pages/QuoteDetail.js
@@ -1,14 +1,26 @@
 import { Route } from "react-router-dom";
 import { useParams } from "react-router";
 
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
 
-const QuoteDetail = (props) => {
+const DUMMY_QUOTES = [
+  {id: 'q1', author: 'Lance', text: 'Learning React is fun'},
+  {id: 'q1', author: 'LanceA', text: 'Learning React is great'},
+]
+
+const QuoteDetail = () => {
   const params = useParams();
+
+  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
+
+  if (!quote) {
+    return <p>No quote found</p>
+  }
+
   return (
     <>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
