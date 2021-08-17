168f64fa29b19a7b282e1b0f4688f0f32819f56a
LanceStasinski <lance.stasinski@gmail.com>
2021-08-16 20:46:38 -0400

    feat: GET quotes from database

--- router-project/src/pages/QuoteDetail.js
+++ router-project/src/pages/QuoteDetail.js
@@ -1,27 +1,49 @@
+import { useEffect } from "react";
 import { Route, Link, useRouteMatch } from "react-router-dom";
 import { useParams } from "react-router";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
 
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
-
-const DUMMY_QUOTES = [
-  { id: "q1", author: "Lance", text: "Learning React is fun" },
-  { id: "q2", author: "LanceA", text: "Learning React is great" },
-];
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
 const QuoteDetail = () => {
   const match = useRouteMatch();
   const params = useParams();
 
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  const { quoteId } = params;
 
-  if (!quote) {
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className='centered'>{error}</p>
+  }
+
+  if (!loadedQuote.text) {
     return <p>No quote found</p>;
   }
 
   return (
     <>
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
       <Route path={match.path} exact>
         <div className="centered">
           <Link to={`${match.url}/comments`} className="btn--flat">
