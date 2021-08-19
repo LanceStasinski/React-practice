5cf15dd41cdfd83aa1fd66933f93aecaa44c26ef
LanceStasinski <lance.stasinski@gmail.com>
2021-08-19 08:10:39 -0400

    feat: Pre-render pages using getStaticProps and getStaticPaths

--- nextjs-project/pages/[meetupId]/index.js
+++ nextjs-project/pages/[meetupId]/index.js
@@ -11,4 +11,37 @@ const MeetupDetails = () => {
   );
 };
 
+export const getStaticPaths = async () => {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const meetupId = context.params.meetupId; //identify between square brackets in folder name
+  return {
+    props: {
+      meetupData: {
+        image: "https://cdn.mos.cms.futurecdn.net/ntFmJUZ8tw3ULD3tkBaAtf.jpg",
+        id: meetupId,
+        title: "First meetup",
+        address: "Some mountain",
+        description: "First meetup",
+      },
+    },
+  };
+};
+
 export default MeetupDetails;
--- nextjs-project/pages/index.js
+++ nextjs-project/pages/index.js
@@ -18,10 +18,28 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-const HomePage = () => {
-  return (
-      <MeetupList meetups={DUMMY_MEETUPS} />
-  );
+const HomePage = (props) => {
+  return <MeetupList meetups={props.meetups} />;
 };
+
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
+
+export function getStaticProps() {
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+    revalidate: 10 //update information every ten seconds if it were to change
+  };
+}
 
 export default HomePage;
