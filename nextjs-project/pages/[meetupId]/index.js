3a157f04b627700af1ba3b1d6735dd612f97a886
LanceStasinski <lance.stasinski@gmail.com>
2021-08-18 21:15:43 -0400

    feat: Style the dynamic details page using components

---
+++ nextjs-project/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://cdn.mos.cms.futurecdn.net/ntFmJUZ8tw3ULD3tkBaAtf.jpg"
+      title="First Meetup"
+      address="Some mountain"
+      description="First meetup"
+    />
+  );
+};
+
+export default MeetupDetails;
