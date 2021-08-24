152d7d9bf1adc0a9f27bafb91bde8eee3c2d00cb
LanceStasinski <lance.stasinski@gmail.com>
2021-08-24 15:18:07 -0400

    test: Test Greetings component to ensure it displays new text and removes old text when button is clicked

--- testing-practice/src/components/Greeting.test.js
+++ testing-practice/src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
 describe("Greeting component", () => {
@@ -14,5 +15,39 @@ describe("Greeting component", () => {
       exact: false,
     });
     expect(hellowWorldElement).toBeInTheDocument();
+  });
+
+  test("renders it's good to see you if the button was not clicked", () => {
+    render(<Greeting />);
+
+    const outputElement = screen.getByText("It's good to see you", {
+      exact: false,
+    });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("render changed! if the button was clicked", () => {
+    //Arrange
+    render(<Greeting />);
+
+    //Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    //Assert
+    const outputElement = screen.getByText("Changed!", { exact: false });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("first paragraph is removed when button is clicked", () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    const outputElement = screen.queryByText("it's good to see you", {
+      exact: false,
+    });
+    expect(outputElement).toBeNull();
   });
 });
