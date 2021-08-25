a3d49245308605ecfd78e561b938cc3b61afbcaf
LanceStasinski <lance.stasinski@gmail.com>
2021-08-25 14:44:35 -0400

    feat: Successfully add Todos and style the components

--- typescript-react/src/components/NewTodo.tsx
+++ typescript-react/src/components/NewTodo.tsx
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
+import classes from './NewTodo.module.css'
 
-const NewTodo = () => {
+const NewTodo: React.FC<{onAddTodo: (text: string) => void }> = (props) => {
   const inputRef = useRef<HTMLInputElement>(null); // null is the inital value
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
@@ -12,11 +13,11 @@ const NewTodo = () => {
       return;
     }
 
-
+    props.onAddTodo(enteredText)
   };
 
   return (
-    <form onSubmit={submitHandler}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <label htmlFor="text">Todo text</label>
       <input type="text" id="text" ref={inputRef} />
       <button>Add Todo</button>
