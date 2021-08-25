0c3dd2142f555f082d4f1b3e0930c2fca107a380
LanceStasinski <lance.stasinski@gmail.com>
2021-08-25 13:20:18 -0400

    feat: Add Todos component and define props type

--- typescript-react/src/App.tsx
+++ typescript-react/src/App.tsx
@@ -1,9 +1,10 @@
+import Todos from './components/Todos';
 import './App.css';
 
 function App() {
   return (
     <div>
-
+      <Todos items={['Learn React', 'Learn TypeScript']}/>
     </div>
   );
 }
