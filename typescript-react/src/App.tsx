482c3c147d99498816fa05d1dbbfdf547345127d
LanceStasinski <lance.stasinski@gmail.com>
2021-08-25 15:26:31 -0400

    refactor: Refactor using context instead of prop chains

--- typescript-react/src/App.tsx
+++ typescript-react/src/App.tsx
@@ -1,36 +1,16 @@
-import { useState } from "react";
 import Todos from "./components/Todos";
-import Todo from "./models/todo";
 import "./App.css";
 import NewTodo from "./components/NewTodo";
+import TodosContextProvider from "./store/todos-context";
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
 
-  // const todos = [
-  //   new Todo('Learn React'),
-  //   new Todo('Learn TypeScript')
-  // ]
-
-  const addTodoHandler = (text: string) => {
-    const newTodo = new Todo(text);
-
-    setTodos((prevTodos) => {
-      return prevTodos.concat(newTodo);
-    });
-  };
-
-  const removeTodoHandler = (todoId: string) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter((todo) => todo.id !== todoId);
-    });
-  };
 
   return (
-    <div>
-      <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
-    </div>
+    <TodosContextProvider>
+      <NewTodo />
+      <Todos/>
+    </TodosContextProvider>
   );
 }
 
